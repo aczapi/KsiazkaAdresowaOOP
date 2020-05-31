7e35f8030bc71ce9e670ce39ccc88b6271c6540e
Nessy90 <nessy0991@gmail.com>
2020-05-31 17:50:21 +0200

    file PlikTekstowy changed

--- PlikTekstowy.h
+++ PlikTekstowy.h
@@ -20,39 +20,5 @@ public:
 
 };
 
-class PlikZAdresatami : public PlikTekstowy
-{
-
-public:
-
-    PlikZAdresatami(string nazwaPliku) : PlikTekstowy(nazwaPliku) {};
-
-    void dopisz(string tekst)
-    {
-        fstream plikTekstowy;
-        plikTekstowy.open(pobierzNazwePliku().c_str(), ios::app);
-
-        if (plikTekstowy.good() == true)
-        {
-            if (czyPlikJestPusty())
-                plikTekstowy << "To jest poczatek pliku" << endl;
-
-             plikTekstowy << tekst << endl;
-        }
-
-        plikTekstowy.close();
-    }
-};
-
-
-int main()
-{
-    PlikZAdresatami plikZAdresatami("adresaci.txt");
-
-    plikZAdresatami.dopisz("tekst do dopisania");
-
-    return 0;
-}
-
 
 #endif
